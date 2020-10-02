1718d0f6e463474854931f83841c42b918e2dfbe
Nachoxt17 <nacho.man.kpo@hotmail.com>
2020-10-02 19:02:58 -0300

    3rd Commit

--- src/reducers/index.js
+++
@@ -1,4 +0,0 @@
-import {counter,customAsyncData} from './counter';
-import {combineReducers } from  'redux';
-const rootReducer = combineReducers({counter,customAsyncData});
-export default rootReducer;
