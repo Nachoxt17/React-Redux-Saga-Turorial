be324a182351dadb24164a74fac635e14247bf11
Nachoxt17 <nacho.man.kpo@hotmail.com>
2020-10-02 00:28:43 -0300

    2nd Commit

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,15 @@
-import { createStore, applyMiddleware } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
-import rootReducer from "./rootReducer";
-import ReduxThunk from "redux-thunk";
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './rootReducer';
+import ReduxThunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
 
-const middleware = [ReduxThunk];
+const sagaMiddleware = createSagaMiddleware();
+/** const middleware = [ReduxThunk]; */
+const middleware = [sagaMiddleware];
 const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(...middleware))
+    rootReducer,
+    composeWithDevTools(applyMiddleware(...middleware))
 );
 
 export default store;
